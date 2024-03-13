799e33e3a8ad10a6d0a0145ac4234b16075bff04
Hazar can Koçtürk <150735983+hazarkocturk@users.noreply.github.com>
2024-03-13 13:52:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+let btn = document.querySelectorAll(".btn");
+      let screen = document.querySelector(".screen");
+      let history = document.querySelector(".history");
+      let historyArray = [];
+      
+      screen.innerText = "";
+      btn.forEach(function (element) {
+        element.addEventListener("click", function (e) {
+          let input = e.target.innerText;
+          if (input === "=") {
+            let result = eval(screen.innerText);
+            historyArray.push(screen.innerText + " = " + result);
+            screen.innerText = result;
+          } else if (input === "AC") {
+            screen.innerText = "";
+          } else {
+            screen.innerText += input;
+          }
+          history.innerHTML = historyArray.join("<br>");
+        });
+      });
+      window.addEventListener("keydown", function (e) {
+        
+        let input = e.key;
+        if (input === "Enter") {
+          let result = eval(screen.innerText);
+          historyArray.push(screen.innerText + " = " + result);
+          screen.innerText = result;
+        } else if (input === "Escape") {
+          screen.innerText = "";
+        } else if (!isNaN(input) || input === "." || input === "+" || input === "-" || input === "*" || input === "/") {
+        screen.innerText += input;
+        }
+        history.innerHTML = historyArray.join("<br>");
+      });
